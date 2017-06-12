8273f6074028dedc4a286cd54561b4ffcb5d91ac
rafante <rafante2@gmail.com>
2017-06-12 01:21:28 -0300

    Begin of chapter creator system

---
+++ unity/TVTycoon/Assets/Resources/Scripts/Controllers/CameraControl.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class CameraControl : MonoBehaviour
+{
+    public Slider upDownSlider;
+    public Slider leftRightSlider;
+    public Slider rotationSlider;
+    public Camera controlledCam;
+    public float upDownFactor;
+    public float leftRightFactor;
+    public float rotationFactor;
+    private Vector3 resetPosition;
+    private float upDownLast; 
+    private float leftRightLast; 
+
+    // Use this for initialization
+    void Start()
+    {
+        setZeroPosition();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        
+    }
+
+    public void setZeroPosition()
+    {
+        resetPosition = controlledCam.transform.position;
+    }
+
+    public void moveCam(float sliderValue, float factor, Vector3 direction)
+    {
+        float value = sliderValue * factor;
+        Vector3 pos = controlledCam.transform.position;
+        Vector3 newPos = pos;
+        newPos.x =  direction.x == 0 ? pos.x : resetPosition.x + value;
+        newPos.y =  direction.y == 0 ? pos.y : resetPosition.y + value;
+        newPos.z =  direction.z == 0 ? pos.z : resetPosition.z + value;
+        controlledCam.transform.position = newPos;
+    }
+
+    public void moveHorizontally()
+    {
+        Vector3 direction = Vector3.right;
+//        Vector3 direction = controlledCam.transform.right.normalized;
+        moveCam(leftRightSlider.value, leftRightFactor, direction);
+    }
+
+    public void moveVertically()
+    {
+        Vector3 direction = Vector3.forward;
+//        Vector3 direction = controlledCam.transform.forward.normalized;
+        moveCam(upDownSlider.value, upDownFactor, direction);
+    }
+
+    public void rotate()
+    {
+        //controlledCam.transform.Translate(amount * leftRightFactor, 0, 0);
+    }
+
+    public void resetCam()
+    {
+        float upDown = upDownSlider.value;
+        float leftRight = leftRightSlider.value;
+        float rotation = upDownSlider.value;
+
+        upDownSlider.value = 0;
+        leftRightSlider.value = 0;
+        rotationSlider.value = 0;
+
+        controlledCam.transform.position = resetPosition;
+    }
+}
---
+++ unity/TVTycoon/Assets/Resources/Scripts/Controllers/SceneComponent.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneComponent : MonoBehaviour
+{
+
+	public ComponentType type;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
+
+public enum ComponentType
+{
+	ACTOR, FURNITURE, ITEM
+}
